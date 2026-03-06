b1f68f0c47d3dde7838111d92ed5c8235790e965
“GamerCleanVic” <jonassd2000@gmail.com>
2026-03-05 23:19:36 -0500

    Hello World

---
+++ academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+//Isso é um comentário de uma linha
+/*
+    Isso é um comentário
+    de mútiplas
+    linhas.
+ */
+//Próxima aula: 10 - pt01
+public class OlaDevDojo {
+    public static void main(String[] args){
+        System.out.println("Olá DevDojo!");
+    }
+}
